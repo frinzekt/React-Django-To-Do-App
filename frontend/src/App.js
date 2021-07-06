c7a88c1fea61699663df6aa43d9d68f40e53540b
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-07-07 01:00:24 +0800

    refactored entire frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,218 +1,202 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+const BACKEND_API_URL = "http://127.0.0.1:8000/api";
+const App = () => {
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-      this.state = {
-        todoList:[],
-        activeItem:{
-          id:null, 
-          title:'',
-          completed:false,
-        },
-        editing:false,
-      }
-      this.fetchTasks = this.fetchTasks.bind(this)
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
-      this.getCookie = this.getCookie.bind(this)
+  // States
+  const [todoList, setTodoList] = useState([]);
+  const [activeItem, setActiveItem] = useState({
+    id: null,
+    title: "",
+    completed: false,
+  });
+  const [editing, setIsEditing] = useState(false);
 
-
-      this.startEdit = this.startEdit.bind(this)
-      this.deleteItem = this.deleteItem.bind(this)
-      this.strikeUnstrike = this.strikeUnstrike.bind(this)
-  };
-
-  getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
+  // Don't mind this
+  // just security stuff
+  const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
+      }
     }
     return cookieValue;
-}
+  };
 
-  componentWillMount(){
-    this.fetchTasks()
-  }
+  //Reload the fetch tasks once upon loading
+  useEffect(() => {
+    fetchTasks(); // will run asynchronously in the background
+  }, []);
 
-  fetchTasks(){
-    console.log('Fetching...')
+  // READ OPERATION
+  const fetchTasks = async () => {
+    console.log("Fetching Data");
+    const response = await fetch(`${BACKEND_API_URL}/task-list/`);
+    const data = await response.json();
+    setTodoList(data);
+  };
 
-    fetch('http://127.0.0.1:8000/api/task-list/')
-    .then(response => response.json())
-    .then(data => 
-      this.setState({
-        todoList:data
-      })
-      )
-  }
+  // FORM HANDLERS HERE
+  const handleChange = (event) => {
+    // Refreshes the state of the active item
+    setActiveItem({ ...activeItem, title: event.target.value });
+  };
 
-  handleChange(e){
-    var name = e.target.name
-    var value = e.target.value
-    console.log('Name:', name)
-    console.log('Value:', value)
+  // CREATE/EDIT OPERATION
+  const handleSubmit = async (event) => {
+    // prevent page reloads
+    event.preventDefault();
 
-    this.setState({
-      activeItem:{
-        ...this.state.activeItem,
-        title:value
-      }
-    })
-  }
+    // Dont mind this... security stuff
+    const csrftoken = getCookie("csrftoken");
 
-  handleSubmit(e){
-    e.preventDefault()
-    console.log('ITEM:', this.state.activeItem)
-
-    var csrftoken = this.getCookie('csrftoken')
-
-    var url = 'http://127.0.0.1:8000/api/task-create/'
-
-    if(this.state.editing == true){
-      url = `http://127.0.0.1:8000/api/task-update/${ this.state.activeItem.id}/`
-      this.setState({
-        editing:false
-      })
+    let url = `${BACKEND_API_URL}/task-create/`;
+    if (editing == true) {
+      url = `${BACKEND_API_URL}/task-update/${activeItem.id}/`;
+      setIsEditing(false);
     }
 
-
-
-    fetch(url, {
-      method:'POST',
-      headers:{
-        'Content-type':'application/json',
-        'X-CSRFToken':csrftoken,
+    // Performs the request
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrftoken,
       },
-      body:JSON.stringify(this.state.activeItem)
-    }).then((response)  => {
-        this.fetchTasks()
-        this.setState({
-           activeItem:{
-          id:null, 
-          title:'',
-          completed:false,
-        }
-        })
-    }).catch(function(error){
-      console.log('ERROR:', error)
-    })
+      body: JSON.stringify(activeItem),
+    });
 
-  }
+    // Refresh the tasks
+    // Not the best way to do it (should use the returned data to append)
+    await fetchTasks();
 
-  startEdit(task){
-    this.setState({
-      activeItem:task,
-      editing:true,
-    })
-  }
+    // Sets no active item
+    setActiveItem({
+      id: null,
+      title: "",
+      completed: false,
+    });
+  };
 
+  // EDIT FORM HANDLER
+  const startEdit = (task) => {
+    setActiveItem(task);
+    setIsEditing(true);
+  };
 
-  deleteItem(task){
-    var csrftoken = this.getCookie('csrftoken')
+  // DELETE OPERATION
+  const deleteItem = async (task) => {
+    const csrftoken = getCookie("csrftoken");
 
-    fetch(`http://127.0.0.1:8000/api/task-delete/${task.id}/`, {
-      method:'DELETE',
-      headers:{
-        'Content-type':'application/json',
-        'X-CSRFToken':csrftoken,
+    const response = await fetch(`${BACKEND_API_URL}/task-delete/${task.id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrftoken,
       },
-    }).then((response) =>{
+    });
 
-      this.fetchTasks()
-    })
-  }
+    // Refresh the tasks
+    // Not the best way to do it (should use the returned data to remove)
+    await fetchTasks();
+  };
 
+  // EDIT OPERATION
+  const strikeToggle = async (task) => {
+    task.completed = !task.completed;
+    const csrftoken = getCookie("csrftoken");
 
-  strikeUnstrike(task){
+    const response = await fetch(`${BACKEND_API_URL}/task-update/${task.id}/`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+      body: JSON.stringify({ completed: task.completed, title: task.title }),
+    });
 
-    task.completed = !task.completed
-    var csrftoken = this.getCookie('csrftoken')
-    var url = `http://127.0.0.1:8000/api/task-update/${task.id}/`
+    // Refresh the tasks
+    // Not the best way to do it (should use the returned data to edit)
+    await fetchTasks();
+  };
 
-      fetch(url, {
-        method:'POST',
-        headers:{
-          'Content-type':'application/json',
-          'X-CSRFToken':csrftoken,
-        },
-        body:JSON.stringify({'completed': task.completed, 'title':task.title})
-      }).then(() => {
-        this.fetchTasks()
-      })
+  return(
+    <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form onSubmit={handleSubmit} id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input
+                    onChange={handleChange}
+                    className="form-control"
+                    id="title"
+                    value={activeItem.title}
+                    type="text"
+                    name="title"
+                    placeholder="Add task.."
+                  />
+                </div>
 
-    console.log('TASK:', task.completed)
-  }
-
-
-  render(){
-    var tasks = this.state.todoList
-    var self = this
-    return(
-        <div className="container">
-
-          <div id="task-container">
-              <div  id="form-wrapper">
-                 <form onSubmit={this.handleSubmit}  id="form">
-                    <div className="flex-wrapper">
-                        <div style={{flex: 6}}>
-                            <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Add task.." />
-                         </div>
-
-                         <div style={{flex: 1}}>
-                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
-                          </div>
-                      </div>
-                </form>
-             
+                <div style={{ flex: 1 }}>
+                  <input
+                    id="submit"
+                    className="btn btn-warning"
+                    type="submit"
+                    name="Add"
+                  />
+                </div>
               </div>
-
-              <div  id="list-wrapper">         
-                    {tasks.map(function(task, index){
-                      return(
-                          <div key={index} className="task-wrapper flex-wrapper">
-
-                            <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
-
-                                {task.completed == false ? (
-                                    <span>{task.title}</span>
-
-                                  ) : (
-
-                                    <strike>{task.title}</strike>
-                                  )}
-  
-                            </div>
-
-                            <div style={{flex:1}}>
-                                <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
-                            </div>
-
-                            <div style={{flex:1}}>
-                                <button onClick={() => self.deleteItem(task)} className="btn btn-sm btn-outline-dark delete">-</button>
-                            </div>
-
-                          </div>
-                        )
-                    })}
-              </div>
+            </form>
           </div>
-          
+
+          <div id="list-wrapper">
+            {todoList.map(function (task, index) {
+              return (
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div
+                    onClick={() =>strikeToggle(task)}
+                    style={{ flex: 7 }}
+                  >
+                    {task.completed == false ? (
+                      <span>{task.title}</span>
+                    ) : (
+                      <strike>{task.title}</strike>
+                    )}
+                  </div>
+
+                  <div style={{ flex: 1 }}>
+                    <button
+                      onClick={() =>startEdit(task)}
+                      className="btn btn-sm btn-outline-info"
+                    >
+                      Edit
+                    </button>
+                  </div>
+
+                  <div style={{ flex: 1 }}>
+                    <button
+                      onClick={() =>deleteItem(task)}
+                      className="btn btn-sm btn-outline-dark delete"
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      )
-  }
-}
-
-
+      </div>
+  )
+};
 
 export default App;
